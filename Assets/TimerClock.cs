59480d03616d35dd1dd2274442c8a35a5d27b670
Filip Walczak <filipwalczak98@gmail.com>
2023-02-05 12:30:08 +0100

    Fixed clock

--- Assets/TimerClock.cs
+++ Assets/TimerClock.cs
@@ -6,7 +6,7 @@ public class TimerClock : MonoBehaviour
     private TextMeshProUGUI label;
 
     private float timer;
-    
+
     private void Awake()
     {
         label = GetComponent<TextMeshProUGUI>();
@@ -16,7 +16,7 @@ public class TimerClock : MonoBehaviour
     {
         timer += Time.deltaTime;
         float seconds = (int)timer % 60;
-        float minutes = Mathf.FloorToInt(seconds / 60);
+        float minutes = Mathf.FloorToInt(timer / 60);
         
         label.text = $"{minutes:00}:{seconds:00}";
     }
